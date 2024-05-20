55006b0a3d143b0e0199f888e119f383a1400a04
dennis <meinemailds@gmail.com>
2024-05-20 17:42:19 +0200

    Refactor data module
    Utilize RecordingSession class (as well as preprocessing module) to hide away BrainFlow logic and act as interface for data access

--- biofeedback/data/data_offline.py
+++ biofeedback/data/data_offline.py
@@ -9,52 +9,124 @@ from brainflow.data_filter import DataFilter
 from brainflow.data_filter import FilterTypes
 from brainflow.data_filter import DetrendOperations
 
-board_id = BoardIds.CYTON_BOARD
-sampling_rate = BoardShim.get_sampling_rate(board_id)
-eeg_channels = BoardShim.get_eeg_channels(board_id)[:-1] # drop last channel (as it is ECG)
-ecg_channel = [BoardShim.get_ecg_channels(board_id).pop()] # last channel is ECG
+import data.preprocessing as prepro
 
-# TODO Clean this up
-#filepaths = []
+board_id_cyton = BoardIds.CYTON_BOARD
+sampling_rate_cyton = BoardShim.get_sampling_rate(board_id_cyton)
+timestamp_channel_cyton = [BoardShim.get_timestamp_channel(board_id_cyton)]
+#package_num_channel_cyton = BoardShim.get_package_num_channel(board_id_cyton)
+eeg_channels_cyton = BoardShim.get_eeg_channels(board_id_cyton)[:-1] # drop last channel (as it is ECG)
+ecg_channel_cyton = [BoardShim.get_ecg_channels(board_id_cyton).pop()] # last channel is ECG
+
+# cutoffs (in samples) to exclude recording setup/cool-down noise
+# TODO determine these params visually!! (same for all recordings)
+session_begin_cutoff = 120 * sampling_rate_cyton 
+session_end_cutoff = 10 * sampling_rate_cyton
+
+
+# TODO Move data into repository
+
 path_dir_01 = r'C:\\Users\\dennis\\Documents\\OpenBCI_GUI\\Recordings\\off_proband_01_2024-02-01_19-31-18'
 name_file_01 = 'BrainFlow-RAW_2024-02-01_19-31-18_0.csv'
-#filepaths.append(os.path.join(path_dir_01, name_file_01))
+session_id_01 = "off_proband_01"
+
 path_dir_02 = r'C:\\Users\\dennis\\Documents\\OpenBCI_GUI\\Recordings\\off_proband_02_2024-02-02_12-11-33'
 name_file_02 = 'BrainFlow-RAW_2024-02-02_12-11-33_0.csv'
-#filepaths.append(os.path.join(path_dir_02, name_file_02))
+session_id_02 = "off_proband_02"
+
 path_dir_03 = r'C:\\Users\\dennis\\Documents\\OpenBCI_GUI\\Recordings\\off_proband_03_2024-02-03_18-07-41'
 name_file_03 = 'BrainFlow-RAW_2024-02-03_18-07-41_0.csv'
-#filepaths.append(os.path.join(path_dir_03, name_file_03))
+session_id_03 = "off_proband_03"
+
 path_dir_04 = r'C:\\Users\\dennis\\Documents\\OpenBCI_GUI\\Recordings\\off_proband_04_2024-02-05_21-16-18'
 name_file_04 = 'BrainFlow-RAW_2024-02-05_21-16-18_0.csv'
-#filepaths.append(os.path.join(path_dir_04, name_file_04))
+session_id_04 = "off_proband_04"
+
 path_dir_05 = r'C:\\Users\\dennis\\Documents\\OpenBCI_GUI\\Recordings\\off_proband_05_2024-02-09_11-30-48'
 name_file_05 = 'BrainFlow-RAW_2024-02-09_11-30-48_0.csv'
-#filepaths.append(os.path.join(path_dir_05, name_file_05))
+session_id_05 = "off_proband_05"
 
-def getFilepaths():
 
+
+# TODO wie steuert das Interface hier die parametrisierung der einzelnen Recording Objekte????
+# TODO Data Segmentation und Cutoff ist Problem des F-Eval Scripts 
+def get_offline_recording_sessions():
+    """get_offline_recording_sessions provides interface to access all offline
+    recording sessions (als interface für das ganze script)
+    - kapselt brainflow logik (daten format und BF preprocessing) weg
+    """
+    
     filepaths = []
-    filepaths.append(os.path.join(path_dir_01, name_file_01))
-    filepaths.append(os.path.join(path_dir_02, name_file_02))
-    filepaths.append(os.path.join(path_dir_03, name_file_03))
-    filepaths.append(os.path.join(path_dir_04, name_file_04))
-    filepaths.append(os.path.join(path_dir_05, name_file_05))
+    filepaths.append((session_id_01, os.path.join(path_dir_01, name_file_01)))
+    filepaths.append((session_id_02, os.path.join(path_dir_02, name_file_02)))
+    filepaths.append((session_id_03, os.path.join(path_dir_03, name_file_03)))
+    filepaths.append((session_id_04, os.path.join(path_dir_04, name_file_04)))
+    filepaths.append((session_id_05, os.path.join(path_dir_05, name_file_05)))
 
-    return filepaths
+    offline_recordings = []
 
-class RecordingData:
-    def __init__(self, filepath):
+    #for (id, path) in filepaths:
+    #    offline_recordings.append(RecordingSession(filepath=path, session_id=id, sampling_rate=sampling_rate_cyton))
+    
+    # FOR TEST PURPOSES 
+    path = (filepaths[1])[1]
+    id = (filepaths[1])[0]
+    offline_recordings.append(RecordingSession(filepath=path, session_id=id, sampling_rate=sampling_rate_cyton))
+
+    return offline_recordings
+
+
+class RecordingSession:
+    """ kapselt BrainFlow weg (als interface zu den Daten/einzelnen Recording Sessions) und
+    provided accesu zu preprocessed (brainflow filter) daten 
+    """
+    def __init__(self, filepath, session_id, sampling_rate):
         self.filepath = filepath
-        # load OpenBCI recording (CSV-file in BrainFlow Format)
+        # load OpenBCI recording (CSV-file in BrainFlow Format) and cutoff begin/end segment
         self.loaded_data =  DataFilter.read_file(filepath)
-        self.eeg_data = self.loaded_data[eeg_channels, :]
-        self.ecg_data = self.loaded_data[ecg_channel, :]
+        self.eeg_data = self.loaded_data[eeg_channels_cyton, session_begin_cutoff:-session_end_cutoff]
+        self.ecg_data = self.loaded_data[ecg_channel_cyton, session_begin_cutoff:-session_end_cutoff]
+        self.timestamps = self.loaded_data[timestamp_channel_cyton, session_begin_cutoff:-session_end_cutoff]
+        self.sampling_rate = sampling_rate
+        self.session_id = session_id
 
-    def getECG(self):
+        # TODO ist das so sinnvoller??
+        data_channels_cyton = ecg_channel_cyton + eeg_channels_cyton
+        self.data = self.loaded_data[data_channels_cyton, session_begin_cutoff:-session_end_cutoff]
+
+    # TODO Check BrainFLow Format ob hier noch transposed werden muss oder nicht!!! Stutze Array auf 1D??
+    def get_ECG_data(self):
+        """get_ECG_data returns data 2D !!! (See BrainFLow for Format)
+        """
          # BrainFlow returns uV!!
         return self.ecg_data
     
-    def getEEG(self):
+    # TODO Check BrainFLow Format ob hier noch transposed werden muss oder nicht!!! stutze Array auf 2D??
+    def get_EEG_data(self):
+        """get_EEG_data returns data 2D !!! (See BrainFLow for Format)
+        """
          # BrainFlow returns uV!!
         return self.eeg_data
+    
+    # Check Format und stutze auf 1D Array
+    def get_preprocessed_ECG_data(self):
+        return prepro.preprocess_data(self.ecg_data)
+
+    # Check Format und stzte auf 2D Array
+    def get_preprocessed_EEG_data(self):
+        return prepro.preprocess_data(self.eeg_data)
+    
+    # TODO macht das Unterschied ob ich EEG UND ECG oder nur EEG preprocesse etc??
+    def get_preprocessed_data(self):
+        return prepro.preprocess_data(self.data)
+    
+    # TODO BRAUCHEN WIR HIER AUCH NOCH TIME STAMPS?
+    
+    def get_description(self):
+        descript = {
+            "session_id" : self.session_id,
+            "sampling_rate" : self.sampling_rate,
+        }
+        return descript
+
+
--- biofeedback/data/preprocessing.py
+++ biofeedback/data/preprocessing.py
@@ -8,21 +8,34 @@ from brainflow.data_filter import FilterTypes
 from brainflow.data_filter import DetrendOperations
 
 
-board_id = BoardIds.CYTON_BOARD
-sampling_rate = BoardShim.get_sampling_rate(board_id)
+board_id_cyton = BoardIds.CYTON_BOARD
+sampling_rate_cyton = BoardShim.get_sampling_rate(board_id_cyton)
 
 
-""" _summary_ 
-    TODO description
-"""
-def preprocessData(data_2D):
 
-    # perform filtering for each row ( = channel) of 2D data array
+
+
+# TODO PreProcess Parametrisierung von außen (aka durch F-Eval Script) erlauben (?)
+# TODO Check BRainFLow Daten Format und Nötiges Daten Format für Preprocessing ob hier noch transposed werden muss oder nicht!!
+def preprocess_data(data_2D):
+    """preprocessData interne hilfsfunktion für BrainFlow processing von 2D numpy array data (according to BrainFlow output)
+
+    Args:
+        data_2D (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    # perform filtering for each row (= channel) of 2D data array
     for channel in range(data_2D.shape[0]):
         # filters work in-place
+        
+        # TODO WELCHES FILTERING HIER SINNVOLL FÜR ANWENDUNGSFALL (ICA for ECG from EEG)
+        # (+ Quellen!!)
         DataFilter.detrend(data_2D[channel], DetrendOperations.CONSTANT.value)
-        DataFilter.perform_bandpass(data_2D[channel], sampling_rate, 3.0, 45.0, 2, FilterTypes.BUTTERWORTH.value, 0)
-        DataFilter.perform_bandstop(data_2D[channel], sampling_rate, 48.0, 52.0, 2, FilterTypes.BUTTERWORTH.value, 0)
-        DataFilter.perform_bandstop(data_2D[channel], sampling_rate, 58.0, 62.0, 2, FilterTypes.BUTTERWORTH.value, 0)
+        DataFilter.perform_bandpass(data_2D[channel], sampling_rate_cyton, 3.0, 45.0, 2, FilterTypes.BUTTERWORTH.value, 0)
+        DataFilter.perform_bandstop(data_2D[channel], sampling_rate_cyton, 48.0, 52.0, 2, FilterTypes.BUTTERWORTH.value, 0)
+        DataFilter.perform_bandstop(data_2D[channel], sampling_rate_cyton, 58.0, 62.0, 2, FilterTypes.BUTTERWORTH.value, 0)
     
     return data_2D
